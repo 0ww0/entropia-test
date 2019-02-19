cb006536658f4da88fb2015a48d14f124a883557
Mazkt Rake <mazkt.rake@gmail.com>
2019-02-19 16:39:17 +0800

    Update gulp with babel

---
+++ gulpfile.babel.js
@@ -0,0 +1,186 @@
+/* File: gulpfile.js */
+
+// Use localhost:8080
+
+'use strict';
+
+import gulp          from 'gulp';
+import babel         from 'gulp-babel';
+import del           from 'del';
+import browserSync   from 'browser-sync';
+import sass          from 'gulp-sass';
+import autoprefixer  from 'gulp-autoprefixer';
+import cssnano       from 'gulp-cssnano';
+import rename        from 'gulp-rename';
+import pug           from 'gulp-pug';
+import prettify      from 'gulp-prettify';
+import newer         from 'gulp-newer';
+import imagemin      from 'gulp-imagemin';
+import concat        from 'gulp-concat';
+import plumber       from 'gulp-plumber';
+import notify        from 'gulp-notify';
+import uglify        from 'gulp-uglify';
+import purgecss      from 'gulp-purgecss';
+import jsimport      from 'gulp-js-import';
+import zip           from 'gulp-zip';
+
+const portDest = '8080',
+    server = browserSync.create(),
+    paths = {
+        build: 'build/',
+        styles: {
+            src: 'src/assets/styles/**/*.scss',
+            dest: 'build/assets/styles/'
+        },
+        scripts: {
+            src: 'src/assets/scripts/**/*.js',
+            srcMain: 'src/assets/scripts/main.js',
+            exSrc: '!src/assets/scripts/components/*.js',
+            dest: 'build/assets/scripts/',
+        },
+        templates: {
+            src: 'src/templates/**/*.+(pug|html)',
+            exSrc: '!src/templates/include/**/*.+(pug|html)',
+            dest: 'build/'
+        },
+        images: {
+            src: 'src/assets/images/**/*.+(jpg|jpeg|png|gif|svg)',
+            exSrc: '!src/assets/images/favicon/**/*',
+            dest: 'build/assets/images/'
+        },
+        favicons: {
+            src: 'src/assets/images/favicon/**/*.+(ico|png)',
+            dest: 'build/'
+        },
+        fonts: {
+            src: 'src/assets/fonts/**/*.+(eot|ttf|svg|woff)',
+            dest: 'build/assets/fonts/'
+        },
+        zip: {
+            src: 'src/**/*',
+            build: 'build/**/*',
+            destOld: 'zip/old',
+            destProd: 'zip/prod'
+        },
+};
+
+export const clean = () => del([paths.build]);
+
+export function reload(done) {
+  server.reload();
+  done();
+}
+
+export function browser(done) {
+    return server.init({
+        server: {
+            baseDir: paths.build
+        },
+        port: portDest
+    });
+    done();
+}
+
+export const templates = () => gulp.src([paths.templates.src, paths.templates.exSrc])
+    .pipe(pug({
+        pretty: true,
+        defaults: {
+            cache: false
+        },
+        verbose: true
+    }))
+    .pipe(prettify({
+        indent_inner_html: true,
+        indent_size: 4,
+        unformatted: []
+    }))
+    .pipe(gulp.dest( paths.templates.dest ))
+
+export const styles = () => gulp.src(paths.styles.src)
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest( paths.styles.dest ))
+    .pipe(cssnano({
+        zindex: false
+    }))
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest( paths.styles.dest ))
+
+export const images = () => gulp.src([ paths.images.src, paths.images.exSrc ])
+    .pipe(newer( paths.images.dest ))
+    .pipe(imagemin([
+        imagemin.jpegtran({
+            progressive: true
+        }),
+        imagemin.optipng({
+            optimizationLevel: 5
+        })
+    ], {
+        verbose: true
+    }))
+    .pipe(gulp.dest( paths.images.dest ))
+
+export const scripts = () => gulp.src([ paths.scripts.src, paths.scripts.exSrc ])
+    .pipe(gulp.dest( paths.scripts.dest ))
+
+export const scriptComps = () => gulp.src(paths.scripts.srcMain)
+    .pipe(jsimport({ hideConsole: false }))
+    .pipe(concat( 'main.js' ))
+    .pipe(gulp.dest( paths.scripts.dest ))
+    .pipe(uglify())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest( paths.scripts.dest ))
+
+export const favicons = () => gulp.src( paths.favicons.src )
+    .pipe(gulp.dest( paths.favicons.dest ))
+
+export const fonts = () => gulp.src( paths.fonts.src )
+     .pipe(gulp.dest( paths.fonts.dest ))
+
+export const zipsource = () => gulp.src( paths.zip.src )
+    .pipe(zip('src-' + Date.now() +'.zip' ))
+    .pipe(gulp.dest( paths.zip.destOld ))
+
+export const zipbuild = () => gulp.src( paths.zip.build )
+    .pipe(zip('build-' + Date.now() +'.zip' ))
+    .pipe(gulp.dest( paths.zip.destOld ))
+
+export const zipprod = () => gulp.src( paths.zip.build )
+    .pipe(zip('build-prod-' + Date.now() +'.zip' ))
+    .pipe(gulp.dest( paths.zip.destProd ))
+
+export const devwatch = () => {
+    gulp.watch( paths.styles.src, gulp.series(styles, reload) );
+    gulp.watch( paths.templates.src, gulp.series(templates, reload) );
+    gulp.watch( paths.images.src, gulp.series(images, reload) );
+    gulp.watch( paths.scripts.src, gulp.series(scripts, scriptComps, reload) );
+    gulp.watch( paths.favicons.src, gulp.series(favicons, reload) );
+    gulp.watch( paths.fonts.src, gulp.series(fonts, reload) );
+};
+
+exports.clean       = clean;
+exports.styles      = styles;
+exports.templates   = templates;
+exports.browser     = browser;
+exports.images      = images;
+exports.scripts     = scripts;
+exports.scriptComps = scriptComps;
+exports.favicons    = favicons;
+exports.fonts       = fonts;
+exports.zipsource   = zipsource;
+exports.zipbuild    = zipbuild;
+exports.zipprod     = zipprod;
+exports.devwatch    = devwatch;
+
+const build = gulp.series( zipsource, zipbuild, clean, templates, styles, scripts, scriptComps, favicons, fonts, images, gulp.parallel( browser, devwatch ));
+
+export const prod = gulp.series(zipprod);
+
+export default build;
---
+++ readme.md
@@ -0,0 +1,73 @@
+# Gulp templates
+
+- Gulp version: 4.0.0
+- Html Preprosessor: pug
+- CSS Preprosessor: scss
+- Javascript Framework: jQuery, Babel 7
+
+Make sure you have NodeJS installed (currently built using v4^).
+
+1. Clone the repo.
+2. `cd` into the folder and do a fresh `npm install`.
+3. Run `gulp serve` to start the server
+
+Server Configuration is at `localhost:8080`!
+
+The port `:8080` will auto changes if detected other Gulp / NodeJS instances.
+
+When serve.
+- The file/folder will delete the folder first and create a new one.
+- The jQuery been combine using gulp task on scriptComps task - gulp-js-import
+
+Devicon Fonts Repo (http://konpa.github.io/devicon/)
+
+Gulp JS Import Issues
+- Not properly passing directory at @import argument between Unix/Linux and Windows.
+- Workaround
+    - Unix/Linux : `./{FolderName}/{FileName}.js`
+    - Windows: `src\assets\{FolderName}\{FolderName}\{FileName}.js`
+
+The Gulp Nodes to be intalled
+
+```{nodejs}
+npm i gulp --save-dev === https://www.npmjs.com/package/gulp
+npm i del --save-dev === https://www.npmjs.com/package/del
+npm i gulp-sass --save-dev === https://www.npmjs.com/package/gulp-sass
+npm i gulp-autoprefixer --save-dev === https://www.npmjs.com/package/gulp-autoprefixer
+npm i gulp-cssnano --save-dev === https://www.npmjs.com/package/gulp-cssnano
+npm i gulp-rename --save-dev === https://www.npmjs.com/package/gulp-rename
+npm i gulp-pug --save-dev === https://www.npmjs.com/package/gulp-pug
+npm i gulp-prettify --save-dev === https://www.npmjs.com/package/gulp-prettify
+npm i browser-sync --save-dev === https://www.npmjs.com/package/browser-sync
+npm i gulp-newer --save-dev === https://www.npmjs.com/package/gulp-newer
+npm i gulp-imagemin --save-dev === https://www.npmjs.com/package/gulp-imagemin
+npm i gulp-concat --save-dev === https://www.npmjs.com/package/gulp-concat
+npm i gulp-plumber --save-dev === https://www.npmjs.com/package/gulp-plumber
+npm i gulp-notify --save-dev === https://www.npmjs.com/package/gulp-notify
+npm i gulp-uglify --save-dev === https://www.npmjs.com/package/gulp-uglify
+npm i gulp-zip --save-dev === https://www.npmjs.com/package/gulp-zip
+npm i gulp-purgecss --save-dev === https://www.npmjs.com/package/gulp-purgecss
+npm i gulp-js-import --save-dev === https://www.npmjs.com/package/gulp-js-import
+npm i gulp-size --save-dev === https://www.npmjs.com/package/gulp-size
+npm i gulp-babel --save-dev === https://www.npmjs.com/package/gulp-babel
+```
+
+```{BabelJS}
+npm i @babel/core --save-dev === https://www.npmjs.com/package/@babel/core
+npm i @babel/polyfill --save-dev === https://www.npmjs.com/package/@babel/polyfill
+npm i @babel/preset-env --save-dev === https://www.npmjs.com/package/@babel/preset-env
+npm i @babel/register --save-dev === https://www.npmjs.com/package/@babel/register
+npm i babel-loader --save-dev === https://www.npmjs.com/package/babel-loader
+```
+
+Express Install All Gulp Nodes
+
+```
+npm i gulp gulp-cli del gulp-sass gulp-autoprefixer gulp-cssnano gulp-rename gulp-pug gulp-prettify browser-sync gulp-newer gulp-imagemin gulp-concat gulp-plumber gulp-notify gulp-uglify gulp-purgecss gulp-js-import gulp-zip gulp-babel --save-dev
+```
+
+Express Install All BabelJS Nodes
+
+```
+npm i @babel/core @babel/polyfill @babel/preset-env @babel/register babel-loader --save-dev
+```
