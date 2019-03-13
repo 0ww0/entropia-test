d66256bd8bc681989ac17a1658a643dc56f49e46
Mazkt Rake <mazkt.rake@gmail.com>
2019-03-13 15:38:46 +0800

    Update Header Scrolling Effect

--- src/assets/scripts/component/_header.js
+++ src/assets/scripts/component/_header.js
@@ -1,4 +1,15 @@
 (function() {
+    var  $dataHeaderScroll = 'data-scroll';
+    $win.scroll(function(){
+        var $this = $(this),
+            $topPos = $this.scrollTop(),
+            $dataScroll = $('[' + $dataHeaderScroll + ']');
 
+        if($topPos > 80){
+            $dataScroll.attr($dataHeaderScroll, 'scroll')
+        } else {
+            $dataScroll.attr($dataHeaderScroll, '')
+        }
+    })
 
 })();
--- src/assets/scripts/component/_scroll.js
+++ src/assets/scripts/component/_scroll.js
@@ -1,10 +1,5 @@
 (function(){
 
-    var $dp = ($(window).width() < 1025),
-        $md = ($(window).width() < 801),
-        $sm = ($(window).width() < 641),
-        $xs = ($(window).width() < 481);
-
     $('a[href*="#"]')
         .not('[href="#"]')
         .not('[href="#0"]')
@@ -14,55 +9,16 @@
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 event.preventDefault();
-                if($dp){
-                    $('html, body').animate({
-                        scrollTop: target.offset().top - 75
-                    }, 1000);
-                }
-                else if($md){
-                    $('html, body').animate({
-                        scrollTop: target.offset().top - 82
-                    }, 1000);
-                }
-                else if($sm){
-                    $('html, body').animate({
-                        scrollTop: target.offset().top - 82
-                    }, 1000);
-                }
-                else if($xs){
-                    $('html, body').animate({
-                        scrollTop: target.offset().top - 75
-                    }, 1000);
-                }
-                else{
-                    $('html, body').animate({
-                        scrollTop: target.offset().top - 82
-                    }, 1000);
-                }
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 100
+                }, 1000);
             }
         }
     });
     $(window).on('resize', function(e){
-        $dp = ($(window).width() < 1025);
-        $md = ($(window).width() < 801);
-        $sm = ($(window).width() < 641);
-        $xs = ($(window).width() < 481);
-    });
-})();
-
-(function(){
-    $(document).ready(function() {
-        $('html, body').hide();
-
-        if (window.location.hash) {
-            setTimeout(function() {
-                $('html, body').scrollTop(0).show();
-                $('html, body').animate({
-                    scrollTop: $(window.location.hash).offset().top - 72
-                    }, 1000)
-            }, 0);
-        }        else {
-            $('html, body').show();
-        }
+        // $dp = ($(window).width() < 1025);
+        // $md = ($(window).width() < 801);
+        // $sm = ($(window).width() < 641);
+        // $xs = ($(window).width() < 481);
     });
 })();
